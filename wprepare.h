ef984ecdf23a82730e22c13416e8c584fa27761b
pewniak747 <pewniak747@gmail.com>
2009-12-13 14:46:39 +0100

    cleaned wprepare

--- wprepare.h
+++ wprepare.h
@@ -1,16 +1,13 @@
-/*******************************************************************************/
-/** dicto v 1.3 WPrepare class header file                                    **/
-/** This file is published under GNU/GPL licence                              **/
-/** http://www.gnu.org/licenses/gpl-3.0.txt                                   **/
-/** author: Tomasz Pewiński "pewniak747"                                      **/
-/** contact: pewniak747@gmail.com                                             **/
-/** http://dicto.sourceforge.net                                              **/
-/*******************************************************************************/
+// dicto v 1.3 WPrepare class header file
+// This file is published under GNU/GPL licence
+// http://www.gnu.org/licenses/gpl-3.0.txt
+// author: Tomasz Pewiński "pewniak747"
+// contact: pewniak747@gmail.com
+// http://pewniak747.github.com/dicto
 
 #ifndef WPREPARE_H
 #define WPREPARE_H
 
-#include <QWidget>
 #include <QLabel>
 #include <QPushButton>
 #include <QComboBox>
@@ -18,35 +15,31 @@
 #include <QSpinBox>
 #include <QSlider>
 
-class WPrepare : public QWidget {
-    Q_OBJECT
+class WPrepare : public QWidget
+{
+	Q_OBJECT
 
-public:
-    WPrepare(QWidget *parent = 0, bool exam=false);
+	public:
+		WPrepare(QWidget *parent = 0, bool exam = false);
 
-private:
-    QLabel *numberLabel;
-    QLabel *intoforeignLabel;
-    QLabel *includeLabel;
-    QLabel *ignoreLabel;
+	private:
+		QLabel *numberLabel;
+		QSlider *numberSlider;
+		QSpinBox *numberBox;
+		QLabel *intoforeignLabel;
+		QComboBox *intoforeignCombo;
+		QLabel *includeLabel;
+		QCheckBox *includeBox;
+		QLabel *ignoreLabel;
+		QCheckBox *ignoreBox;
+		QPushButton *startButton;
+		QPushButton *cancelButton;
 
-    QSlider *numberSlider;
-    QSpinBox *numberBox;
-    QCheckBox *includeBox;
-    QCheckBox *ignoreBox;
-    QComboBox *intoforeignCombo;
-
-    QPushButton *startButton;
-    QPushButton *cancelButton;
-
-    void closeEvent(QCloseEvent * a);
-
-private slots:
-    void startTest();
-    void startExam();
-    void cancel();
-    void updateSlider();
-	void includeChanged();
+	private slots:
+		void startTest();
+		void startExam();
+		void includeChanged(int state);
+		void closeEvent(QCloseEvent * a);
 };
-
+	
 #endif // WPREPARE_H
