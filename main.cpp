dfcfe15e7e26295236c538770372c1f3f2add1ce
pewniak747 <pewniak747@gmail.com>
2009-12-14 16:22:01 +0100

    cleaned main

--- main.cpp
+++ main.cpp
@@ -1,52 +1,45 @@
-/*******************************************************************************/
-/** dicto v 1.3 main file                                                     **/
-/** This file is published under GNU/GPL licence                              **/
-/** http://www.gnu.org/licenses/gpl-3.0.txt                                   **/
-/** author: Tomasz Pewiński "pewniak747"                                      **/
-/** contact: pewniak747@gmail.com                                             **/
-/** http://dicto.sourceforge.net                                              **/
-/*******************************************************************************/
-
-/**svn test**/
+// dicto v 1.3 main file
+// This file is published under GNU/GPL licence
+// http://www.gnu.org/licenses/gpl-3.0.txt
+// author: Tomasz Pewiński "pewniak747"
+// contact: pewniak747@gmail.com
+// http://pewniak747.github.com/dicto
 
 #include <QtGui>
 #include <QApplication>
 #include "wmain.h"
 #include <iostream>
 
-    QApplication *application;
-
-    WMain *wMain;
+QApplication *application;
+WMain *wMain;
 
 int main(int argc, char *argv[]) {
-    application = new QApplication (argc, argv);
-    
-    //some localisation mess
-    QTranslator translator;
-	translator.load("./dicto_pl");
+	application = new QApplication (argc, argv);
+	
+		// set codecs
+	QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
+	QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+	QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
+	
+	// localisation mess
+	QTranslator translator;
+	translator.load("./translations/dicto_pl");
 	QString locale=application->tr("%1").arg(QLocale::system().name());
-	//locale="eng";
-	if(locale == "pl_PL") {
-		application->installTranslator(&translator);
-	}
-
-    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
-    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
-    QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
-
-    wMain=new WMain;
-    wMain->show();
-    
-    if(argc > 1) {
+	if(locale == "pl_PL") application->installTranslator(&translator);
+	
+	// create main window
+	wMain=new WMain;
+	
+	//parse argv
+	if(argc > 1) {
 		QDir dir;
-		QString fname = argv[1];
-		
-		if(fname[0] != '/' && (fname.size() > 1 && fname[1] != ':')) {
-			fname.prepend(dir.absolutePath()+"/");
-		}
-		std::cout << "Opening file: " << fname.toStdString() << "\n";
+		QString filename = argv[1];
+		if(fname[0] != '/' && (fname.size() > 1 && fname[1] != ':')) 
+			filename.prepend(dir.absolutePath()+"/");
 		if(!fname.isEmpty()) wMain->cDocument->readFromFile(fname);
 	}
-    
-    return application->exec();
+	
+	//show main window
+	wMain->show();
+	return application->exec();
 }
